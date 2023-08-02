b50fff927bb6740cb39037a19c6f4d2ca8bd6ea4
Evan Luo <contact@ohevan.com>
2023-08-01 22:26:52 -0400

    animation faster

--- app/page.tsx
+++ app/page.tsx
@@ -29,8 +29,8 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1 className="z-10 text-5xl text-transparent duration-700 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        evannotfound
+      <h1 className=" text-center z-10 text-5xl text-transparent duration-700 bg-white cursor-default animate-title text-edge-outline font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+        evan<span className={"block sm:inline"}>notfound</span>
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
