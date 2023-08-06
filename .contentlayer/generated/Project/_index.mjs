ceab1c03b8ad28253e1a872c5475e2ed7a54c63e
Evan Luo <contact@ohevan.com>
2023-08-06 18:52:45 -0400

    add linkscape

--- .contentlayer/generated/Project/_index.mjs
+++ .contentlayer/generated/Project/_index.mjs
@@ -3,8 +3,9 @@
 import projects__blogMdx from './projects__blog.mdx.json' assert { type: 'json' }
 import projects__hexoThemeRedefineMdx from './projects__hexo-theme-redefine.mdx.json' assert { type: 'json' }
 import projects__jcsufNextMdx from './projects__jcsuf-next.mdx.json' assert { type: 'json' }
+import projects__linkscapeSiteMdx from './projects__linkscape-site.mdx.json' assert { type: 'json' }
 import projects__photographyMdx from './projects__photography.mdx.json' assert { type: 'json' }
 import projects__typoraThemeRedefineMdx from './projects__typora-theme-redefine.mdx.json' assert { type: 'json' }
 import projects__vercelHexoTemplateMdx from './projects__vercel-hexo-template.mdx.json' assert { type: 'json' }
 
-export const allProjects = [projects__blogMdx, projects__hexoThemeRedefineMdx, projects__jcsufNextMdx, projects__photographyMdx, projects__typoraThemeRedefineMdx, projects__vercelHexoTemplateMdx]
+export const allProjects = [projects__blogMdx, projects__hexoThemeRedefineMdx, projects__jcsufNextMdx, projects__linkscapeSiteMdx, projects__photographyMdx, projects__typoraThemeRedefineMdx, projects__vercelHexoTemplateMdx]
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,17 +1,21 @@
-import { PropsWithChildren } from "react";
+import type { MDXComponents } from "mdx/types";
+
+// This file allows you to provide custom React components
+// to be used in MDX files. You can import and use any
+// React component you want, including components from
+// other libraries.
 
 // This file is required to use MDX in `app` directory.
-export function useMDXComponents(components: any): any {
-	return {
-		// Allows customizing built-in components, e.g. to add styling.
-		h1: ({ children }: PropsWithChildren) => (
-			<h1 className="mt-2 text-3xl font-bold tracking-tight text-zinc-100 md:text-center sm:text-4xl">
-				{children}
-			</h1>
-		),
-		h2: ({ children }: PropsWithChildren) => (
-			<h2 className="text-zinc-50">{children}</h2>
-		),
-		...components,
-	};
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    // Allows customizing built-in components, e.g. to add styling.
+    // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
+    h1: ({ children }) => (
+      <h1 className="mt-2 text-3xl font-bold tracking-tight text-zinc-100 md:text-center sm:text-4xl">
+        {children}
+      </h1>
+    ),
+    h2: ({ children }) => <h2 className="text-zinc-50">{children}</h2>,
+    ...components,
+  };
 }
